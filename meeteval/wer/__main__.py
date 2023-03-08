6e55f79e168a0e53c70d961be9ef31f7624f8d1d
Thilo von Neumann <tvn@mail.upb.de>
2023-03-08 11:23:20 +0100

    Switch back to argparse instead of click
    - Support for globbing on shell level, e.g., -r ref*.stm

--- meeteval/wer/__main__.py
+++ meeteval/wer/__main__.py
@@ -1,13 +1,14 @@
+import argparse
 import dataclasses
+import itertools
 import json
 import io
 import os
 import glob
-from functools import wraps
 from pathlib import Path
-from typing import Tuple, List, Optional
+from typing import List
 
-from meeteval.io.ctm import CTMGroup, CTM
+from meeteval.io.ctm import CTMGroup
 from meeteval.io.stm import STM
 from meeteval.wer.wer import (
     cp_word_error_rate,
@@ -19,10 +20,8 @@ from meeteval.wer.wer import (
 )
 import sys
 
-import click
 
-
-def _dump(obj, path: Path):
+def _dump(obj, path: Path, default_suffix='.json'):
     """
     Dumps the `obj` to `path`. Parses the suffix to find the file type.
     When a suffix is missing, use json.
@@ -33,18 +32,19 @@ def _dump(obj, path: Path):
     else:
         p = path.open('w')
     with p as fd:
-        if path.suffix == '.json' or path.suffix == '':
+        suffix = path.suffix
+        if suffix == '':
+           suffix = default_suffix
+        if suffix == '.json':
             json.dump(obj, fd, indent=2, sort_keys=False)
-        elif path.suffix == '.yaml':
+        elif suffix == '.yaml':
             import yaml
             yaml.dump(obj, fd, sort_keys=False)
         else:
-            raise NotImplemented(f'Unknown file ext: {path.suffix}')
+            raise NotImplemented(f'Unknown file ext: {suffix}')
 
     if path.stem != '-':
         print(f'Wrote: {path}', file=sys.stderr)
-    else:
-        print()
 
 
 def _load(path: Path):
@@ -58,12 +58,12 @@ def _load(path: Path):
             raise NotImplemented(f'Unknown file ext: {path.suffix}')
 
 
-def _load_reference(reference: Path):
+def _load_reference(reference: [Path, str, io.TextIOBase, tuple, list]):
     """Loads a reference transcription file. Currently only STM supported"""
     return STM.load(reference)
 
 
-def _load_hypothesis(hypothesis: Tuple[Path, ...]):
+def _load_hypothesis(hypothesis: List[Path]):
     """Loads the hypothesis. Supports one STM file or multiple CTM files
     (one per channel)"""
     if len(hypothesis) > 1:
@@ -78,8 +78,6 @@ def _load_hypothesis(hypothesis: Tuple[Path, ...]):
             filename = hypothesis.name
         else:
             filename = str(hypothesis)
-
-    # print(type(hypothesis), repr(hypothesis), hypothesis, filename)
 
     if filename.endswith('.ctm'):
         if isinstance(hypothesis, list):
@@ -96,17 +94,29 @@ def _load_hypothesis(hypothesis: Tuple[Path, ...]):
         raise RuntimeError(hypothesis, filename)
 
 
-def _load_texts(reference, hypothesis):
+def _load_texts(reference_paths, hypothesis_paths):
     """Load and validate reference and hypothesis texts.
 
     Validation checks that reference and hypothesis have the same example IDs.
     """
+    # Normalize and glob (for backwards compatibility) the path input
+    reference_paths = list(itertools.chain.from_iterable(reference_paths))
+    hypothesis_paths = list(itertools.chain.from_iterable(hypothesis_paths))
+
+    def _glob(pathname):
+        match = list(glob.glob(pathname))
+        assert match, (pathname, match)
+        return match
+
+    reference_paths = [Path(file) for r in reference_paths for file in _glob(r)]
+    hypothesis_paths = [Path(file) for h in hypothesis_paths for file in _glob(h)]
+
     # Load input files
-    reference = _load_reference(reference)
+    reference = _load_reference(reference_paths)
 
     # Hypothesis can be an STM file or a collection of  CTM files. Detect
     # which one we have and load it
-    hypothesis = _load_hypothesis(hypothesis)
+    hypothesis = _load_hypothesis(hypothesis_paths)
 
     # Group by example IDs
     reference = reference.grouped_by_filename()
@@ -114,27 +124,17 @@ def _load_texts(reference, hypothesis):
 
     # Check that the input is valid
     if reference.keys() != hypothesis.keys():
-        if len(reference.keys()) != len(hypothesis.keys()):
-            raise RuntimeError(
-                f'Length of reference ({len(reference)}) and hypothesis ({len(hypothesis)}) should be equal.')
-        else:
-            raise RuntimeError(
-                'Keys of reference and hypothesis differ\n'
-                f'hypothesis - reference: e.g. {list(set(hypothesis.keys()) - set(reference.keys()))[:5]}'
-                f'reference - hypothesis: e.g. {list(set(reference.keys()) - set(hypothesis.keys()))[:5]}'
-            )
+        raise RuntimeError(
+            'Keys of reference and hypothesis differ\n'
+            f'hypothesis - reference: e.g. {list(set(hypothesis.keys()) - set(reference.keys()))[:5]}\n'
+            f'reference - hypothesis: e.g. {list(set(reference.keys()) - set(hypothesis.keys()))[:5]}'
+        )
 
-    return reference, hypothesis
+    return reference, reference_paths, hypothesis, hypothesis_paths
 
 
-def _get_parent_stem(hypothesis_paths):
-
-    hypothesis_paths = [
-        Path(p.name).resolve()
-        if isinstance(p, io.TextIOBase) else
-        Path(p).resolve()
-        for p in hypothesis_paths
-    ]
+def _get_parent_stem(hypothesis_paths: List[Path]):
+    hypothesis_paths = [p.resolve() for p in hypothesis_paths]
 
     if len(hypothesis_paths) == 1:
         parent, stem = hypothesis_paths[0].parent, hypothesis_paths[0].stem
@@ -145,21 +145,14 @@ def _get_parent_stem(hypothesis_paths):
         prefix = os.path.commonprefix(stems)
         postfix = os.path.commonprefix([s[::-1] for s in stems])[::-1]
 
-        print('parent', parent)
-        print('prefix', prefix)
-        print('postfix', postfix)
-        print('hypothesis_paths', hypothesis_paths)
         stem = f'{prefix}{postfix}'
-
-    parent = str(parent).replace('*', '').replace('?', '')
-    stem = stem.replace('*', '').replace('?', '')
 
     return parent, stem
 
 
 def _save_results(
         per_reco,
-        hypothesis_paths: Path,
+        hypothesis_paths: List[Path],
         wer_suffix: str,
         per_reco_out: str,
         average_out: str,
@@ -168,7 +161,6 @@ def _save_results(
     """
 
     parent, stem = _get_parent_stem(hypothesis_paths)
-
 
     average_output = Path(average_out.format(
         parent=f'{parent}/',
@@ -189,174 +181,150 @@ def _save_results(
     }, per_reco_output)
 
     # Compute and save average
-    _average(per_reco.values(), average_output)
+    average = combine_error_rates(*per_reco.values())
+    _dump(dataclasses.asdict(average), average_output)
 
 
-def _print_human_readable(d):
-    from pprint import pprint
-    for k, v in d.items():
-        print(f'{k}: ', end='')
-        pprint(v)
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+parser.add_argument('--version', action='store_true', help='Show version')
 
+commands = parser.add_subparsers(title='Subcommands')
 
-def _average(error_rates: List[ErrorRate], out: Path, print_human_readable=False):
-    average = combine_error_rates(*error_rates)
-    if print_human_readable:
-        _print_human_readable(dataclasses.asdict(average))
-    _dump(dataclasses.asdict(average), out)
-
-
-@click.group(help='MeetEval meeting evaluation tool')
-def cli():
-    pass
-
-
-average_out_option = click.option(
-    '--average-out', default='{parent}/{stem}_{wer}.json',
+common_wer_parser = argparse.ArgumentParser(add_help=False)
+common_wer_parser.add_argument(
+    '-r', '--reference', help='Reference file in STM format',
+    nargs='+',
+    action='append',
+    # type=argparse.FileType('r'),
+)
+common_wer_parser.add_argument(
+    '-h', '--hypothesis', help='Hypothesis file in STM format',
+    # type=argparse.FileType('r'),
+    nargs='+',
+    action='append',
+)
+common_wer_parser.add_argument(
+    '--average-out',
+    default='{parent}/{stem}_{wer}.json',
     help='Output file for the average file. {stem} is replaced with the stem '
          'of the (first) hypothesis file and {wer} is replaced with the wer'
          'type (equal to the command name). '
          '"-" is interpreted as stdout. For example: "-.yaml" prints to '
          'stdout in yaml format.'
 )
+common_wer_parser.add_argument(
+    '--per-reco-out', default='{parent}/{stem}_{wer}_per_reco.json',
+    help='Output file for the per_reco file. {stem} is replaced with the stem '
+         'of the (first) hypothesis file and {wer} is replaced with the wer'
+         'type (equal to the command name). '
+)
+
+common_merge_parser = argparse.ArgumentParser(add_help=False)
+common_merge_parser.add_argument(
+    '-o', '--out',
+    required=False, default='-',
+    # type=argparse.FileType('w')
+)
+common_merge_parser.add_argument(
+    'files', nargs='+',
+    # type=argparse.FileType('r')
+)
 
 
-def wer_command(help=None):
-    def _wer_command(f):
-        @cli.command(help=help)
-        @click.option(
-            '--reference', '-r', required=True, multiple=True,
-            # type=click.File('r'),
-            type=click.Path(exists=False, dir_okay=False),
-            help='The reference file. Currently only support STM file(s). '
-                 'To use glob you have to escape the string, '
-                 'e.g. -r "eval*.stm"'
+def command(name, help, parent_parsers):
+    def _command(fn):
+        command_parser = commands.add_parser(
+            name,
+            add_help=False,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            help=help,
+            parents=parent_parsers,
         )
-        @click.option(
-            '--hypothesis', '-h', multiple=True, required=True,
-            # type=click.File('r'),
-            type=click.Path(exists=False, dir_okay=False),
-            help='The hypothesis file(s). Support either STM file(s) or'
-                 'multiple CTM files (one per hypothesis output)'
+        command_parser.add_argument(
+            '--help', help='show this help message and exit',
+            action='help',
+            default=argparse.SUPPRESS,
         )
-        @click.option(
-            '--per-reco-out', default='{parent}/{stem}_{wer}_per_reco.json',
-            help='Output file for the per_reco file. {stem} is replaced with the stem '
-                 'of the (first) hypothesis file and {wer} is replaced with the wer'
-                 'type (equal to the command name). '
+        command_parser.set_defaults(func=fn)
+        return fn
 
+    return _command
+
+
+@command(
+    'orcwer',
+    help='Computes the Optimal Reference Combination Word Error Rate (ORC WER)',
+    parent_parsers=[common_wer_parser],
+)
+def orcwer(reference, hypothesis, average_out, per_reco_out):
+    reference, _, hypothesis, hypothesis_paths = _load_texts(reference, hypothesis)
+    _save_results({
+        example_id: orc_word_error_rate(
+            reference=reference[example_id].utterance_transcripts(),
+            hypothesis={
+                k: h.merged_transcripts()
+                for k, h in hypothesis[example_id].grouped_by_speaker_id().items()
+            },
         )
-        @average_out_option
-        @wraps(f)
-        def f_command(reference: 'list[str]', hypothesis: 'list[str]', per_reco_out, average_out, **kwargs):
-            def _glob(pathname):
-                match = list(glob.glob(pathname))
-                assert match, (pathname, match)
-                return match
-
-            reference_path = reference
-            hypothesis_path = hypothesis
-
-            reference = [
-                file for r in reference for file in _glob(r)]
-            hypothesis = [
-                file for h in hypothesis for file in _glob(h)]
-
-            reference, hypothesis = _load_texts(reference, hypothesis)
-
-            # Compute cpWER for all examples
-            per_reco = {}
-            for example_id in reference.keys():
-                per_reco[example_id] = f(
-                    reference=(reference[example_id]),
-                    hypothesis=(hypothesis[example_id]),
-                    **kwargs
-                )
-            _save_results(per_reco, hypothesis_path, f_command.name, per_reco_out, average_out)
-
-        return f_command
-
-    return _wer_command
+        for example_id in reference.keys()
+    }, hypothesis_paths, 'orcwer', per_reco_out, average_out)
 
 
-@wer_command(
+@command(
+    'cpwer',
     help='Computes the Concatenated minimum-Permutation Word Error Rate '
-         '(cpWER)'
+         '(cpWER)',
+    parent_parsers=[common_wer_parser],
 )
-def cpwer(reference, hypothesis):
-    return cp_word_error_rate(
-        reference={k: r_.merged_transcripts() for k, r_ in reference.grouped_by_speaker_id().items()},
-        hypothesis={k: h_.merged_transcripts() for k, h_ in hypothesis.grouped_by_speaker_id().items()},
-    )
+def cpwer(reference, hypothesis, average_out, per_reco_out):
+    reference, _, hypothesis, hypothesis_paths = _load_texts(reference, hypothesis)
+    _save_results({
+        example_id: cp_word_error_rate(
+            reference={
+                k: r.merged_transcripts()
+                for k, r in reference[example_id].grouped_by_speaker_id().items()
+            },
+            hypothesis={
+                k: h.merged_transcripts()
+                for k, h in hypothesis[example_id].grouped_by_speaker_id().items()
+            },
+        )
+        for example_id in reference.keys()
+    }, hypothesis_paths, 'cpwer', per_reco_out, average_out)
 
 
-@wer_command(
-    help='Computes the Optimal Reference Combination Word Error Rate (ORC WER)'
+@command(
+    'mimower',
+    help='Computes the MIMO WER',
+    parent_parsers=[common_wer_parser],
 )
-def orcwer(reference, hypothesis):
-    return orc_word_error_rate(
-        reference=reference.utterance_transcripts(),
-        hypothesis={k: h_.merged_transcripts() for k, h_ in hypothesis.grouped_by_speaker_id().items()},
-    )
+def mimower(reference, hypothesis, average_out, per_reco_out):
+    reference, _, hypothesis, hypothesis_paths = _load_texts(reference, hypothesis)
+    _save_results({
+        example_id: mimo_word_error_rate(
+            reference={
+                k: r.utterance_transcripts()
+                for k, r in reference[example_id].grouped_by_speaker_id().items()
+            },
+            hypothesis={
+                k: h.merged_transcripts()
+                for k, h in hypothesis[example_id].grouped_by_speaker_id().items()
+            },
+        )
+        for example_id in reference.keys()
+    }, hypothesis_paths, 'mimower', per_reco_out, average_out)
 
 
-@wer_command(
-    help='Computes the MIMO WER'
-)
-def mimower(reference, hypothesis):
-    return mimo_word_error_rate(
-        reference={k: r_.utterance_transcripts() for k, r_ in reference.grouped_by_speaker_id().items()},
-        hypothesis={k: h_.merged_transcripts() for k, h_ in hypothesis.grouped_by_speaker_id().items()},
-    )
-
-
-@cli.command(
-    help='Computes the average WER over a per-reco file'
-)
-@click.argument(
-    'files', required=True,
-    nargs=-1,
-    type=click.Path(exists=True, dir_okay=False, allow_dash=True, path_type=Path),
-)
-@click.option(
-    '-o', '--out', required=False, default='-',
-    # type=click.Path(writable=True, allow_dash=True, path_type=Path),
-    type=click.File(mode='w', atomic=True),
-)
-def merge(files: 'List[bytes]', out: 'io.TextIOBase' = None):
-    return _merge(**locals(), average=None)
-
-
-def _merge(files: 'List[bytes | str | Path]', out: 'io.TextIOBase' = None, average: bool = None):
-    files = [
-        Path(f.decode())  # CB: Why is this on my system bytes and on GitHub Actions a Path?
-        if isinstance(f, bytes) else
-        Path(f)
-        for f in files
-    ]
-    if out.name == '<stdout>':
-        file_ext = list({file.suffix for file in files})
-        assert len(file_ext) == 1, (file_ext, files)
-        file_ext, = file_ext
-        assert file_ext in ['.json', '.yaml'], (file_ext, files)
-    else:
-        # print(repr(out), out, out.name)
-        file_ext = Path(out.name).suffix
-        assert file_ext in ['.json', '.yaml'], (file_ext, out)
-
-    def load(file: Path):
-        if file.suffix == '.json':
-            return json.loads(file.read_text())
-        elif file.suffix == '.json':
-            import yaml
-            return yaml.load(file)
-        else:
-            raise RuntimeError(file)
-
-    data = [load(f) for f in files]
-
-    # types = [average_or_per_reco(d) for d in data]
-    # average = False
+def _merge(
+        files: List[str],
+        out: str = None,
+        average: bool = None
+):
+    # Load input files
+    files = [Path(f) for f in files]
+    data = [_load(f) for f in files]
 
     import meeteval
     ers = []
@@ -381,32 +349,46 @@ def _merge(files: 'List[bytes | str | Path]', out: 'io.TextIOBase' = None, avera
         }
         assert len(out_data) == len(ers), (len(out_data), len(ers), 'Duplicate filenames')
 
-    if file_ext == '.json':
-        out_data = json.dumps(out_data, indent=2, sort_keys=False)
-    elif file_ext == '.yaml':
-        import yaml
-        out_data = yaml.dump(out_data)
-    else:
-        raise RuntimeError(file_ext, 'Cannot happen.')
-
-    print(out_data, file=out)
+    _dump(out_data, Path(out), default_suffix=files[0].suffix)
 
 
-@cli.command(
-    help='Computes the average WER over a per-reco file'
+@command(
+    'merge',
+    help='Merges multiple (per-reco or averaged) files',
+    parent_parsers=[common_merge_parser]
 )
-@click.argument(  # CB: Why does this return bytes?
-    'files', required=True,
-    nargs=-1,
-    type=click.Path(exists=True, dir_okay=False, allow_dash=True, path_type=Path),
+def merge(files, out):
+    return _merge(files, out, average=None)
+
+
+@command(
+    'average',
+    help='Computes the average over one or multiple per-reco files',
+    parent_parsers=[common_merge_parser]
 )
-@click.option(
-    '-o', '--out', required=False, default='-',
-    # type=click.Path(writable=True, allow_dash=True, path_type=Path),
-    type=click.File(mode='w', atomic=True),
-)
-def average(files: 'List[bytes]', out: 'io.TextIOBase' = None):
-    _merge(files, out, average=True)
+def average(files, out):
+    return _merge(files, out, average=True)
+
+
+def cli():
+    # Parse arguments and find command to execute
+    args = parser.parse_args()
+
+    if hasattr(args, 'func'):
+        kwargs = vars(args)
+        fn = args.func
+        # Pop also removes from args namespace
+        kwargs.pop('func')
+        kwargs.pop('version')
+        fn(**kwargs)
+        return
+
+    if getattr(args, 'version', False):
+        from meeteval import __version__
+        print(__version__)
+        return
+
+    parser.print_help()
 
 
 if __name__ == '__main__':
