a9d5372572368fc5b7cc729cbfc506a579361676
Thilo von Neumann <tvn@mail.upb.de>
2023-08-14 12:41:14 +0200

    Import stm functions in inits

--- meeteval/wer/wer/__init__.py
+++ meeteval/wer/wer/__init__.py
@@ -1,6 +1,6 @@
-from .cp import cp_word_error_rate, CPErrorRate
-from .mimo import mimo_word_error_rate, MimoErrorRate
+from .cp import cp_word_error_rate, CPErrorRate, cp_word_error_rate_stm
+from .mimo import mimo_word_error_rate, MimoErrorRate, mimo_word_error_rate_stm
 from .orc import orc_word_error_rate, orc_word_error_rate_stm, OrcErrorRate
-from .siso import siso_word_error_rate, siso_character_error_rate
+from .siso import siso_word_error_rate, siso_character_error_rate, siso_word_error_rate_keyed_text
 from .error_rate import ErrorRate, combine_error_rates
-from .time_constrained import time_constrained_minimum_permutation_word_error_rate, time_constrained_siso_word_error_rate
+from .time_constrained import time_constrained_minimum_permutation_word_error_rate, time_constrained_siso_word_error_rate, tcp_word_error_rate_stm
