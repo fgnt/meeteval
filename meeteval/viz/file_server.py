870e3fcc3aac70d16f07501ac26c8ecb0e3f5a64
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2024-01-29 13:05:00 +0100

    viz: file server: support slice, catch all exceptions

--- meeteval/viz/file_server.py
+++ meeteval/viz/file_server.py
@@ -1,16 +1,39 @@
 """
-Deliver normalized audio files.
+A file server, that exposes wav files from the filesystem:
+ - Only delivers wav files
+ - Normalizes the audio files (Useful for far files, that is typically low volumn)
+ - Supports slicing
 
 python -m meeteval.viz.file_server
 """
 
-from aiohttp import web
-from pathlib import Path
 import io
-import subprocess
+from pathlib import Path
+from aiohttp import web
 
-import soundfile
 import numpy as np
+import soundfile
+
+
+def _parse_audio_slice(path):
+    """
+    See from paderbox.io.audioread.py::_parse_audio_slice for advanced parser.
+
+    >>> _parse_audio_slice('file.wav::[1:2]')
+    ('file.wav', 1, 2)
+    """
+    start = stop = None
+    if '::' in path:
+        path, slice = path.split('::')
+        assert slice[0] == '[' and slice[-1] == ']', slice
+        start, stop = slice[1:-1].split(':')
+        if '.' in start or '.' in stop:
+            samplerate = soundfile.info(path).samplerate
+            start = round(float(start) * samplerate)
+            stop = round(float(stop) * samplerate)
+        else:
+            start, stop = int(start), int(stop)
+    return path, start, stop
 
 
 class Backend:
@@ -18,18 +41,16 @@ class Backend:
         pass
 
     async def handle(self, request: web.Request):
-        print('request:', request)
-        name = request.match_info.get('name', "Anonymous")
-        name = '/' + name
-        # assert name.startswith('/net/'), name
-        name = Path(name)
-        if name.suffix in ['.wav']:
-            try:
-                data, sample_rate = soundfile.read(str(name))
-            except Exception:
-                import traceback
-                traceback.print_exc()
-            else:
+        try:
+            print('request:', request)
+            name = request.match_info.get('name', "Anonymous")
+            name = '/' + name
+            # assert name.startswith('/net/'), name
+            name, start, stop = _parse_audio_slice(name)
+            name = Path(name)
+
+            if name.suffix in ['.wav']:
+                data, sample_rate = soundfile.read(str(name), start=start, stop=stop)
                 data = data * 0.95 / np.amax(np.abs(data))
                 bytes = io.BytesIO()
                 # Wav files produce in some browsers artefacts in the audio.
@@ -45,6 +66,9 @@ class Backend:
                 #     'sox', '--norm', str(name), '-t', 'ogg', '-'
                 # ], stdout=subprocess.PIPE, check=True)
                 # return web.Response(body=cp.stdout)
+        except Exception:
+            import traceback
+            traceback.print_exc()
         return web.Response(status=web.HTTPUnauthorized().status_code)
 
     def main(self):
@@ -53,6 +77,7 @@ class Backend:
                         web.get('/{name:.*}', self.handle),
                         ])
         web.run_app(app, port=7777)
+
 
 if __name__ == '__main__':
     b = Backend()
