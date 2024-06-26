16c136e5414454df4eb425d60f082dbd007ed77f
Thilo von Neumann <tvn@mail.upb.de>
2024-06-26 13:43:13 +0200

    Search for all markdown files and track global state

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -3,10 +3,19 @@ import re
 from pathlib import Path
 import ast
 
+MEETEVAL_ROOT = Path(__file__).parent.parent
+
 FENCED_CODE_BLOCK_REGEX = re.compile(r'```([^`\n]*)?\n((?:.|\n)*?)\n```')
 
 # List of language blocks that are not tested
 LANG_BLACKLIST = ['shell', 'bibtex', '']
+
+KEEP_CONTEXT = ['doc/algorithms.md']
+
+
+@pytest.fixture(scope='session')
+def global_state():
+    return {}
 
 
 def get_fenced_code_blocks(markdown_string: str):
@@ -62,23 +71,31 @@ def exec_with_source(code, filename, lineno, globals_=None, locals_=None):
     exec(compiled, globals_, locals_)
 
 
-README = Path(__file__).parent.parent / 'README.md'
-
-
-@pytest.mark.parametrize('codeblock', get_fenced_code_blocks(README.read_text()))
-def test_readme(codeblock):
+@pytest.mark.parametrize(
+        ('filename', 'codeblock'), 
+        [
+            # pytest.param(filename, codeblock, id=f'{str(filename.relative_to(MEETEVAL_ROOT))}-codeblock{codeblock_index}')
+            (str(filename.relative_to(MEETEVAL_ROOT)), codeblock)
+            for filename in MEETEVAL_ROOT.glob('**/*.md')
+            for codeblock_index, codeblock in enumerate(get_fenced_code_blocks(filename.read_text()))
+        ]
+)
+def test_readme(filename, codeblock, global_state):
     """Run fenced code blocks in readme isolated"""
     import os
-    os.chdir(Path(__file__).parent.parent)
+    os.chdir(MEETEVAL_ROOT)
     lang, code, lineno = codeblock
     if lang in LANG_BLACKLIST:
         return
 
     try:
         if lang == 'python':
-            globals_ = {}
+            if filename in KEEP_CONTEXT:
+                globals_ = global_state.setdefault(filename, {})
+            else:
+                globals_ = {}
             for code, expected_output, line_offset in split_code_block_comment_output(code):
-                exec_with_source(code, str(README), lineno + line_offset, globals_)
+                exec_with_source(code, str(filename), lineno + line_offset, globals_)
                 output = str(globals_.pop('__output', None))
                 if expected_output is not None:
                     # Check that the output is equal to the expected output, but we want to ignore whitespace
