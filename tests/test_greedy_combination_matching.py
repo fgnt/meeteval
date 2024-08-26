6998d7d32ec44ebf81044420938c932ba2d2cc5f
Thilo von Neumann <tvn@mail.upb.de>
2024-08-26 14:44:09 +0200

    Fix tests for greedy ORC-WER

--- tests/test_greedy_combination_matching.py
+++ tests/test_greedy_combination_matching.py
@@ -50,30 +50,26 @@ def test_greedy_bound_by_optimal(segments, streams):
 
 
 def test_example_files_orc_equals_greedy_orc():
-    optimal_result = meeteval.wer.orcwer(example_files / 'ref.stm', example_files / 'hyp.stm')
-    greedy_result = meeteval.wer.greedy_orcwer(example_files / 'ref.stm', example_files / 'hyp.stm')
+    optimal_result = meeteval.wer.orcwer(example_files / 'ref?.stm', example_files / 'hyp?.stm')
+    greedy_result = meeteval.wer.greedy_orcwer(example_files / 'ref?.stm', example_files / 'hyp?.stm')
 
     assert optimal_result == greedy_result
 
 
-@given(segments(10), streams(4), st.sampled_from(['1', '21']))
-def test_optimal_assignment_is_not_changed(segments, streams, distancetype):
+@given(segments(10), streams(4))
+def test_optimal_assignment_is_not_changed(segments, streams):
     """
     Test that the optimal assignment is not changed.
 
     Only works with distancetype='1'. The optimal ORC assignment is not
-    guaranteed to be optimal for other substitution costs than 1.
-    For distanctype='21' we can at least guarantee that the greedily corrected
-    assignment is not worse than the optimal assignment, but we cannot guarantee
-    equality.
+    guaranteed to be optimal for substitution costs other than 1.
     """
     optimal_distance, optimal_assignment = orc_matching.orc_matching_v3(segments, streams)
 
     greedy_distance, greedy_assignment = greedy_combination_matching.greedy_combination_matching(
         segments, streams, optimal_assignment,
-        distancetype=distancetype,
+        distancetype='1',
     )
 
     assert optimal_distance == greedy_distance
-    if distancetype == '1':
-        assert optimal_assignment == greedy_assignment
+    assert optimal_assignment == greedy_assignment
