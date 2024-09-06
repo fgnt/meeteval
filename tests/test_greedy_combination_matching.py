0eb7b5b8bd9ed6252b794966db8e004322a5f0ed
Thilo von Neumann <tvn@mail.upb.de>
2024-09-06 12:31:44 +0200

    Add tests for greedy time-constrained combination matching

--- tests/test_greedy_combination_matching.py
+++ tests/test_greedy_combination_matching.py
@@ -2,11 +2,10 @@ import itertools
 from pathlib import Path
 
 import pytest
-import time
 
 from meeteval.wer.matching import greedy_combination_matching, orc_matching
 import meeteval
-from hypothesis import given, strategies as st, settings, assume
+from hypothesis import given, strategies as st, settings
 
 example_files = (Path(__file__).parent.parent / 'example_files').absolute()
 
@@ -18,31 +17,32 @@ segments = lambda max_size: st.lists(utterance, min_size=0, max_size=max_size)
 streams = lambda max_size: st.lists(utterance, min_size=1, max_size=max_size)
 
 
-def _check_output(distance, assignment, segments, streams):
+def _check_output(distance, assignment, segments, streams, check_distance=True):
     assert isinstance(distance, int)
     assert distance >= 0
     assert len(assignment) == len(segments)
     assert all(isinstance(a, int) for a in assignment)
     assert all(0 <= a < len(streams) for a in assignment)
-    d = orc_matching._levensthein_distance_for_assignment(segments, streams, assignment)
-    assert d == distance, (d, distance, assignment)
+    if check_distance:
+        d = orc_matching._levensthein_distance_for_assignment(segments, streams, assignment)
+        assert d == distance, (d, distance, assignment)
 
 
-@given(segments(10), streams(4))
+@given(segments(10), streams(4), st.sampled_from(['1', '2', '21']))
 @settings(deadline=None)  # The tests take longer on the GitHub actions test servers
-def test_greedy_combination_matching_burn(segments, streams):
+def test_greedy_combination_matching_burn(segments, streams, distancetype):
     """Burn-test. Brute-force is exponential in the number of reference
     utterances, so choose a small number."""
     distance, assignment = greedy_combination_matching.greedy_combination_matching(
-        segments, streams, [0] * len(segments)
+        segments, streams, [0] * len(segments), distancetype=distancetype
     )
-    _check_output(distance, assignment, segments, streams)
+    _check_output(distance, assignment, segments, streams, check_distance=distancetype != '2')
 
 
-@given(segments(6), streams(4))
-def test_greedy_bound_by_optimal(segments, streams):
+@given(segments(6), streams(4), st.sampled_from(['1', '2', '21']))
+def test_greedy_bound_by_optimal(segments, streams, distancetype):
     greedy_distance, _ = greedy_combination_matching.greedy_combination_matching(
-        segments, streams, [0] * len(segments)
+        segments, streams, [0] * len(segments), distancetype=distancetype
     )
 
     optimal_distance, _ = orc_matching.orc_matching_v3(segments, streams)
@@ -134,3 +134,46 @@ def test_greedy_time_constrained_correct(a, b):
     )
 
     assert siso_dist == column[-1]
+
+
+
+utterance_with_timings = st.lists(st.tuples(st.integers(min_value=0, max_value=10), st.floats(allow_nan=False, allow_infinity=False), st.floats(0, 1, allow_nan=False, allow_infinity=False)), min_size=1, max_size=10)
+segments_with_timing = lambda max_size: st.lists(utterance_with_timings, min_size=1, max_size=max_size)
+streams_with_timing = lambda max_size: st.lists(utterance_with_timings, min_size=1, max_size=max_size)
+
+
+@given(segments_with_timing(2), streams_with_timing(2), st.sampled_from(['1', '2', '21']))
+def test_greedy_time_constrained_bound_by_optimal(segments, streams, distancetype):
+    greedy_distance, _ = greedy_combination_matching.greedy_time_constrained_combination_matching(
+        segments, streams, [0] * len(segments), distancetype=distancetype
+    )
+
+    from meeteval.wer.matching.cy_time_constrained_orc_matching import time_constrained_orc_levenshtein_distance
+    optimal_distance, _ = time_constrained_orc_levenshtein_distance(
+        [[s[0] for s in ss] for ss in segments],
+        [[s[0] for s in ss] for ss in streams],
+        [[s[1:] for s in ss] for ss in segments],
+        [[s[1:] for s in ss] for ss in streams],
+    )
+    assert optimal_distance <= greedy_distance
+
+
+@given(segments(4), streams(6), st.sampled_from(['1', '2', '21']))
+def test_greedy_vs_greedy_time_constrained(segments, streams, distancetype):
+    """
+    Tests that the time-constrained version gives the same result as the
+    non-time-constrained version when all words overlap.
+    """
+    greedy_distance, greedy_assignment = greedy_combination_matching.greedy_combination_matching(
+        segments, streams, [0] * len(segments), distancetype=distancetype
+    )
+
+    greedy_time_constrained_distance, greedy_time_constrained_assignment = greedy_combination_matching.greedy_time_constrained_combination_matching(
+        [[(w, 0, 1) for w in segment] for segment in segments],
+        [[(w, 0, 1) for w in stream] for stream in streams],
+        [0] * len(segments),
+        distancetype=distancetype
+    )
+
+    assert greedy_time_constrained_distance == greedy_distance
+    assert greedy_time_constrained_assignment == greedy_assignment
